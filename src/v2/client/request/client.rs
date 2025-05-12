1e2ee5a054b1014260cb9a380ab9f5b04d172696
HakoChest <zoneherobrine@gmail.com>
2025-05-12 20:06:05 +0800

    0.0.58 Adjusted Client Ordering

--- src/v2/client/request/client.rs
+++ src/v2/client/request/client.rs
@@ -1,15 +1,15 @@
+use super::api::oauth::ReqwestOauth;
 use super::api::beatmaps::ReqwestBeatmaps;
 use super::api::beatmapsets::ReqwestBeatmapsets;
 use super::api::changelog::ReqwestChangelog;
+use super::api::chat::ReqwestChat;
+use super::api::comments::ReqwestComments;
 use super::api::events::ReqwestEvents;
+use super::api::forum::ReqwestForum;
 use super::api::matches::ReqwestMatches;
 use super::api::multiplayer::ReqwestMultiplayer;
 use super::api::news::ReqwestNews;
-use super::api::comments::ReqwestComments;
-use super::api::chat::ReqwestChat;
-use super::api::forum::ReqwestForum;
 use super::api::notifications::ReqwestNotifications;
-use super::api::oauth::ReqwestOauth;
 use super::api::ranking::ReqwestRanking;
 use super::api::scores::ReqwestScores;
 use super::api::search::ReqwestSearch;
@@ -23,21 +23,21 @@ use tokio::sync::RwLock;
 #[derive(Clone)]
 pub struct OsynicOsuApiV2Client {
     pub oauth: ReqwestOauth,
-    pub users: ReqwestUsers,
     pub beatmapsets: ReqwestBeatmapsets,
     pub beatmaps: ReqwestBeatmaps,
-    pub events: ReqwestEvents,
-    pub notifications: ReqwestNotifications,
-    pub matches: ReqwestMatches,
-    pub news: ReqwestNews,
-    pub comments: ReqwestComments,
-    pub chat: ReqwestChat,
-    pub forum: ReqwestForum,
-    pub ranking: ReqwestRanking,
-    pub multiplayer: ReqwestMultiplayer,
     pub changelog: ReqwestChangelog,
+    pub chat: ReqwestChat,
+    pub comments: ReqwestComments,
+    pub events: ReqwestEvents,
+    pub forum: ReqwestForum,
     pub search: ReqwestSearch,
+    pub matches: ReqwestMatches,
+    pub multiplayer: ReqwestMultiplayer,
+    pub news: ReqwestNews,
+    pub notifications: ReqwestNotifications,
+    pub ranking: ReqwestRanking,
     pub scores: ReqwestScores,
+    pub users: ReqwestUsers,
     pub wiki: ReqwestWiki,
     pub o_token: Arc<RwLock<OToken>>,
     pub client: reqwest::Client,
@@ -48,10 +48,6 @@ impl OsynicOsuApiV2Client {
         let client = reqwest::Client::new();
         let o_token = Arc::new(RwLock::new(o_token));
         OsynicOsuApiV2Client {
-            users: ReqwestUsers {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
             oauth: ReqwestOauth {
                 client: client.clone(),
                 o_token: o_token.clone(),
@@ -64,23 +60,7 @@ impl OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
-            events: ReqwestEvents {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            notifications: ReqwestNotifications {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            matches: ReqwestMatches {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            news: ReqwestNews {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            comments: ReqwestComments {
+            changelog: ReqwestChangelog {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
@@ -88,19 +68,15 @@ impl OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
+            comments: ReqwestComments {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            events: ReqwestEvents {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
             forum: ReqwestForum {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            ranking: ReqwestRanking {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            multiplayer: ReqwestMultiplayer {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            changelog: ReqwestChangelog {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
@@ -108,7 +84,31 @@ impl OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
+            matches: ReqwestMatches {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            multiplayer: ReqwestMultiplayer {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            news: ReqwestNews {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            notifications: ReqwestNotifications {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            ranking: ReqwestRanking {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
             scores: ReqwestScores {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            users: ReqwestUsers {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
@@ -132,10 +132,6 @@ impl Default for OsynicOsuApiV2Client {
         let client = reqwest::Client::new();
         let o_token = Arc::new(RwLock::new(OToken::default()));
         OsynicOsuApiV2Client {
-            users: ReqwestUsers {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
             oauth: ReqwestOauth {
                 client: client.clone(),
                 o_token: o_token.clone(),
@@ -148,23 +144,7 @@ impl Default for OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
-            events: ReqwestEvents {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            notifications: ReqwestNotifications {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            matches: ReqwestMatches {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            news: ReqwestNews {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            comments: ReqwestComments {
+            changelog: ReqwestChangelog {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
@@ -172,19 +152,15 @@ impl Default for OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
+            comments: ReqwestComments {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            events: ReqwestEvents {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
             forum: ReqwestForum {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            ranking: ReqwestRanking {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            multiplayer: ReqwestMultiplayer {
-                client: client.clone(),
-                o_token: o_token.clone(),
-            },
-            changelog: ReqwestChangelog {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
@@ -192,7 +168,31 @@ impl Default for OsynicOsuApiV2Client {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
+            matches: ReqwestMatches {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            multiplayer: ReqwestMultiplayer {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            news: ReqwestNews {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            notifications: ReqwestNotifications {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            ranking: ReqwestRanking {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
             scores: ReqwestScores {
+                client: client.clone(),
+                o_token: o_token.clone(),
+            },
+            users: ReqwestUsers {
                 client: client.clone(),
                 o_token: o_token.clone(),
             },
